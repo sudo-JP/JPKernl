7aa3e644f4bd90c655d420d908ee7f10d4e390b4
Jason Phan <jase.phan@mail.utoronto.ca>
2026-01-04 23:58:48 -0500

    fixed setcontext

--- src/arch/context.rs
+++ src/arch/context.rs
@@ -32,17 +32,18 @@ pub unsafe extern "C" fn getcontext(old_sp_ptr: *mut *mut u32) {
         "str r7, [r2, #12]",
 
         // Temp registers 
-        "mov r4, r8", 
-        "str r4, [r2, #16]", 
-        "mov r5, r9",
-        "str r5, [r2, #20]",
-        "mov r6, r10", 
-        "str r6, [r2, #24]", 
-        "mov r7, r11",
-        "str r7, [r2, #28]",
+        "mov r1, r8", 
+        "str r1, [r2, #16]", 
+        "mov r1, r9",
+        "str r1, [r2, #20]",
+        "mov r1, r10", 
+        "str r1, [r2, #24]", 
+        "mov r1, r11",
+        "str r1, [r2, #28]",
 
         // Store new stack to r0 
         "str r2, [r0]",
+        "bx lr",
     );
 }
 
@@ -57,12 +58,65 @@ pub fn start_first_process() -> () {
         CURRENT = Some(pid);
 
         // This function should not return 
-        setcontext(process.sp);
+        run_first_process(process.sp);
     }
 
     #[allow(unreachable_code)]
     { panic!("Code should not reach here"); }
 }
+
+#[unsafe(naked)]
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn run_first_process(sp: *const u32) -> ! {
+    core::arch::naked_asm!(
+        // Restore r4-r7 registers from SP (from function arg at r0)
+        "ldr r4, [r0, #0]", 
+        "ldr r5, [r0, #4]", 
+        "ldr r6, [r0, #8]", 
+        "ldr r7, [r0, #12]", 
+
+        // Can't directly do ldr on r8-r11 because thumb only, whatever that means
+        // We use r1 as temporary reg instead
+        "ldr r1, [r0, #16]", 
+        "mov r8, r1", 
+        "ldr r1, [r0, #20]", 
+        "mov r9, r1",
+        "ldr r1, [r0, #24]", 
+        "mov r10, r1",
+        "ldr r1, [r0, #28]", 
+        "mov r11, r1",
+
+        // Advance sp to point to r0 
+        "adds r0, r0, #32", 
+
+        // Set the process sp to r0
+        "msr psp, r0", 
+
+        // Switch to thread mode by writing 1 to control reg
+        // Add on bit index 1 (which is 2)
+        "movs r1, #2",
+
+        // Save back to control
+        "msr CONTROL, r1", 
+
+        // Sync barrier
+        "isb", 
+
+        // Restore special registers, pop from sp to r0-r3
+        // Since in thread mode, and we set sp to psp, this is valid
+        "pop {{r0-r3}}",
+        "pop {{r4}}", 
+        "mov r12, r4",
+
+        "pop {{r4}}",       // LR  
+        "mov lr, r4",
+
+        "pop {{r4, r5}}",   // PC lives in r4 temporarily, discard r5
+
+        "bx r4",
+    );
+}
+
 
 /*
  *
@@ -120,8 +174,8 @@ pub unsafe extern "C" fn setcontext(sp: *const u32) -> ! {
 
         "pop {{r4, r5}}",   // PC lives in r4 temporarily, discard r5
 
-        "bx r4",
-        ""
+        "ldr r0, =0xFFFFFFFD",
+        "bx r0",
     );
 }
 
--- src/arch/interrupts.rs
+++ src/arch/interrupts.rs
@@ -26,12 +26,7 @@ fn handle_alarm() {
     });
 }
 
-
-
-#[interrupt]
-unsafe fn TIMER_IRQ_0() -> ! {
-    handle_alarm();  
-
+fn switch_process() {
     let sched = ptr::addr_of_mut!(SCHEDULER);
     let sleep_q = ptr::addr_of_mut!(SLEEP_QUEUE);
     
@@ -57,7 +52,7 @@ unsafe fn TIMER_IRQ_0() -> ! {
         }
         
         if old_pid == next_pid {
-            switch_context(&mut (*old_pcb), &(*old_pcb));
+            return;
         }
         
         CURRENT = Some(next_pid);
@@ -69,48 +64,17 @@ unsafe fn TIMER_IRQ_0() -> ! {
         
         switch_context(&mut (*old_pcb), &(*new_pcb));
     }
+}
+
+#[interrupt]
+unsafe fn TIMER_IRQ_0() -> ! {
+    handle_alarm();  
+    switch_process();
     loop {}
 }
 
 #[exception]
 unsafe fn PendSV() -> ! {
-    let sched = ptr::addr_of_mut!(SCHEDULER);
-    let sleep_q = ptr::addr_of_mut!(SLEEP_QUEUE);
-    
-    unsafe {
-        let old_pid = CURRENT.unwrap();
-
-        // wake up all sleeping processes 
-        while (*sleep_q).get_size() > 0 {
-            if check_sleep_and_wake().is_err() {
-                break; 
-            }
-        }
-
-        // Get new process
-        let next_pid = (*sched).dequeue().ok().unwrap();
-
-        let old_pcb: *mut PCB = PROCS[old_pid as usize].as_mut().unwrap();
-        match (*old_pcb).state {
-            crate::ProcessState::Ready | crate::ProcessState::Running => {
-                let _ = (*sched).enqueue(old_pid);
-            }
-            _ => {},
-        }
-        let _ = (*sched).enqueue(old_pid);
-        
-        if old_pid == next_pid {
-            switch_context(&mut (*old_pcb), &(*old_pcb));
-        }
-        
-        CURRENT = Some(next_pid);
-        
-        let new_pcb: *mut PCB = PROCS[next_pid as usize].as_mut().unwrap();
-        
-        (*old_pcb).state = crate::ProcessState::Ready;
-        (*new_pcb).state = crate::ProcessState::Running;
-        
-        switch_context(&mut (*old_pcb), &(*new_pcb));
-    }
+    switch_process();
     loop {}
 }
